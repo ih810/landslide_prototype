a66308bfc56d591469813655cbf979aa4a162d57
ih810 <ivanhong810@gmail.com>
2021-12-16 17:57:46 +0800

    16/12 off

--- fn/src/Component/circleChrat.js
+++ fn/src/Component/circleChrat.js
@@ -5,15 +5,15 @@ import { PieChart, Pie, Legend, Tooltip, ResponsiveContainer } from "recharts";
 export default function CircleChart(props) {
     console.log(props)
     return (
-        <ResponsiveContainer width="100%" height="80%">
+        <ResponsiveContainer width="100%" height="100%">
             <PieChart>
-            <Legend wrapperStyle={{ paddingBottom: "10px" }} />
+            <Legend wrapperStyle={{ paddingBottom: "5px" }} />
             <Pie
                 dataKey="value"
                 data={props.data}
                 cx="50%"
-                cy="40%"
-                innerRadius={40}
+                cy="50%"
+                innerRadius={50}
                 outerRadius={80}
             ></Pie>
             <Tooltip />
--- fn/src/Page/newModelSetting.js
+++ fn/src/Page/newModelSetting.js
@@ -23,7 +23,6 @@ export default function NewModelSetting() {
     { value: 10, fill: "#db74d4", name: "Testing Samples Ratio" },
   ]);
 
-
   const handleSampleChange = (e) => {
     let newArr = [...sampleData];
     let value = parseInt(e.target.value);
@@ -56,105 +55,126 @@ export default function NewModelSetting() {
 
   return (
     <>
-        <StepNavBtn title="Train New Model" next="/validateInput"/>
-        <Grid container sx={{ ml: 9, mr: 9 }}>
-          <Grid item xs={8} sx={{ pb: 3, pr: 3 }}>
-            <Grid container sx={gridSpacing} className="sampling-container">
-              <Grid item xs={8} sx={{pl:5, pr:5}} className="sampling-input ">
-                <Paper sx={{ p: 2, pl: 5 }}>
-                  <h3 className="pb-5">SAMPLING</h3>
-                  {[
-                    "Sample Height",
-                    "Sample Width",
-                    "Negative Samples Ratio",
-                    "Low Contrast samples ratio",
-                  ].map((txt, i) => {
-                    return (
-                      <div className={spacebw} key={i}>
-                        <label htmlFor={txt}>{txt}</label>
-                        <input
-                          type="number"
-                          value={sampleData[i].value}
-                          id={txt}
-                          onChange={handleSampleChange}
-                        />
-                      </div>
-                    );
-                  })}
-                </Paper>
-              </Grid>
-              <Grid
-                item
-                xs={4}
-                className="sampling-pie d-flex flex-column justify-content-center align-items-center"
-                sx={{ bgcolor: "#393939", borderRadius: "10px", height: "350px"}}
-              >
-                <div className="pt-3">
-                  <p style={{ color: "#FFF" }}>SAMPLING</p>
-                </div>
-                <CircleChart data={sampleData}/>
-              </Grid>
+      <StepNavBtn title="Train New Model" next="/validateInput" />
+      <Grid container sx={{ ml: 15, mr: 9 }}>
+        <Grid item xs={8} sx={{ pb: 0, pr: 3 }}>
+          <Grid container sx={gridSpacing} className="sampling-container">
+            <Grid
+              item
+              xs={8}
+              sx={{ pl: 5, pr: 5 }}
+              className="sampling-input h-100"
+            >
+              <Paper sx={{ p: 2, pl: 5 }}>
+                <h3 className="pb-5" style={{ fontWeight: 650 }}>
+                  SAMPLING
+                </h3>
+                {[
+                  "Sample Height",
+                  "Sample Width",
+                  "Negative Samples Ratio",
+                  "Low Contrast samples ratio",
+                ].map((txt, i) => {
+                  return (
+                    <div className={spacebw} key={i}>
+                      <label htmlFor={txt}>{txt}</label>
+                      <input
+                        style={{
+                          minWidth: "0px",
+                        }}
+                        type="number"
+                        value={sampleData[i].value}
+                        id={txt}
+                        onChange={handleSampleChange}
+                      />
+                    </div>
+                  );
+                })}
+              </Paper>
             </Grid>
-            <Grid container sx={gridSpacing} className="train-container">
-              <Grid item xs={8} sx={{pl:5, pr:5}} className="train-input">
-                <Paper sx={{ p: 2, pl: 5}}>
-                  <h3 className="pb-5">Train</h3>
-                  {[
-                    "Training Samples Ratio",
-                    "Validate Samples Ratio",
-                    "Test Sample Ratio",
-                  ].map((txt, i) => {
-                    return (
-                      <div className={spacebw} key={i}>
-                        <label htmlFor={txt}>{txt}</label>
-                        <input
-                          type="number"
-                          value={trainData[i].value}
-                          id={txt}
-                          onChange={handleTrainChange}
-                        />
-                      </div>
-                    );
-                  })}
-                </Paper>
-              </Grid>
-              <Grid
-                item
-                xs={4}
-                className="train-pie d-flex flex-column justify-content-center align-items-center"
-                sx={{
-                  bgcolor: "#393939",
-                  borderRadius: "10px",
-                  height: "350px",
-                }}
-              >
-                <div className="pt-3">
-                  <p style={{ color: "#FFF" }}>TRAIN</p>
-                </div>
-                <CircleChart data={trainData}/>
-              </Grid>
+            <Grid
+              item
+              xs={4}
+              className="h-100 sampling-pie d-flex flex-column justify-content-center align-items-center"
+              sx={{ bgcolor: "#393939", borderRadius: "10px", height: "350px" }}
+            >
+              <div className="pt-3">
+                <p style={{ color: "#FFF" }}>SAMPLING</p>
+              </div>
+              <CircleChart data={sampleData} />
             </Grid>
           </Grid>
-          <Grid item xs={4} sx={{ mt: 2 }}>
-            <Paper sx={{ p: 3 }}>
-              {[
-                "Convolutional-Layers",
-                "Dense Layers",
-                "Convolutional Kernel Size",
-                "Max Pooling Layer Size",
-                "Drop Out Rate",
-              ].map((txt, i) => {
-                return (
-                  <div key={i} className="p-3 pl-5 pt-5">
-                    <label htmlFor={txt}>{txt}</label>
-                    <br />
-                    <input type="number" id={txt} />
-                  </div>
-                );
-              })}
-            </Paper>
+          <Grid container sx={gridSpacing} className="train-container">
+            <Grid item xs={8} sx={{ pl: 5, pr: 5 }} className="train-input ">
+              <Paper sx={{ p: 2, pl: 5, height: "100%" }}>
+                <h3 className="pb-5" style={{ fontWeight: 650 }}>
+                  Train
+                </h3>
+                {[
+                  "Training Samples Ratio",
+                  "Validate Samples Ratio",
+                  "Test Sample Ratio",
+                ].map((txt, i) => {
+                  return (
+                    <div className={spacebw} key={i}>
+                      <label htmlFor={txt}>{txt}</label>
+                      <input
+                        style={{
+                          minWidth: "0px",
+                        }}
+                        type="number"
+                        value={trainData[i].value}
+                        id={txt}
+                        onChange={handleTrainChange}
+                      />
+                    </div>
+                  );
+                })}
+              </Paper>
+            </Grid>
+            <Grid
+              item
+              xs={4}
+              className="h-100 train-pie d-flex flex-column justify-content-center align-items-center"
+              sx={{
+                bgcolor: "#393939",
+                borderRadius: "10px",
+                height: "350px",
+              }}
+            >
+              <div className="pt-3">
+                <p style={{ color: "#FFF" }}>TRAIN</p>
+              </div>
+              <CircleChart data={trainData} />
+            </Grid>
           </Grid>
         </Grid>
+        <Grid item xs={4} sx={{ mt: 2 }}>
+          <Paper sx={{ p: 3, height: "100%"}}>
+            {[
+              "Convolutional-Layers",
+              "Dense Layers",
+              "Convolutional Kernel Size",
+              "Max Pooling Layer Size",
+              "Drop Out Rate",
+            ].map((txt, i) => {
+              return (
+                <div key={i} className="p-3 pl-5">
+                  <label htmlFor={txt}>{txt}</label>
+                  <br/>
+                  <input
+                    style={{
+                      minWidth: "10px",
+                    }}
+                    type="number"
+                    id={txt}
+                  />
+                </div>
+              );
+            })}
+          </Paper>
+        </Grid>
+      </Grid>
     </>
   );
 }

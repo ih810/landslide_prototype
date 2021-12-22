3ce76d7b32f0a301833649ac4e4a752e6c150c2f
ih810 <ivanhong810@gmail.com>
2021-12-22 11:26:30 +0800

    progress bar

---
+++ fn/src/Component/progressBar.js
@@ -0,0 +1,42 @@
+import * as React from "react";
+import PropTypes from "prop-types";
+import LinearProgress from "@mui/material/LinearProgress";
+import { styled } from "@mui/material/styles";
+import Typography from "@mui/material/Typography";
+import Box from "@mui/material/Box";
+
+const BorderLinearProgress = styled(LinearProgress)(({ theme }) => ({
+  height: 10,
+  borderRadius: 5,
+}));
+
+function LinearProgressWithLabel(props) {
+  return (
+    <Box sx={{ display: "flex", alignItems: "center" }}>
+      <Box sx={{ width: "100%", mr: 1 }}>
+        <BorderLinearProgress variant="determinate" {...props} />
+      </Box>
+      <Box sx={{ minWidth: 35 }}>
+        <Typography variant="body2" color="text.secondary">
+          {isNaN(props.value) ? "ERROR" : `${Math.round(props.value)}%`}
+        </Typography>
+      </Box>
+    </Box>
+  );
+}
+
+LinearProgressWithLabel.propTypes = {
+  /**
+   * The value of the progress indicator for the determinate and buffer variants.
+   * Value between 0 and 100.
+   */
+  value: PropTypes.number.isRequired,
+};
+
+export default function LinearWithValueLabel(props) {
+  return (
+    <Box sx={{ width: "100%" }}>
+      <LinearProgressWithLabel value={props.progress} color={props.color} />
+    </Box>
+  );
+}

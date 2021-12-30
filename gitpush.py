5e75994ed5e2effab46e0c8126b86d67c3ec4a52
ih810 <ivanhong810@gmail.com>
2021-12-30 10:37:45 +0800

    test

--- gitpush.py
+++ gitpush.py
@@ -7,9 +7,11 @@ def run(*args):
 def main():
     commit_message = input('Commit message: ')
     commit_branch = input('Branch: ') 
+    push = input('Push?: ')
 
     run('add', '.')
     run('commit', '-m', commit_message)
-    run('push', 'origin', commit_branch)
+    if push:
+        run('push', 'origin', commit_branch)
 
 main()

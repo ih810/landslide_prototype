929ec04138a8f4907ba1ef5c73905f5ff724e045
ih810 <ivanhong810@gmail.com>
2021-12-30 14:17:39 +0800

    homepage api done

--- bn/app.py
+++ bn/app.py
@@ -37,35 +37,46 @@ def login():
 
 @app.route('/homepage/admin-dashboard', methods=['GET'])
 def get_all_project():
-    data_list = []
-    azure_list = List('data/')
-    for item in azure_list:
-        data_list.append(item.name)
-    
-    return jsonify(data_list) 
+    user_id = request.args.get('user_id')
+    return user_id 
 
 @app.route('/homepage/user-dashboard', methods=['GET'])
 def get_user_project():
-    data_list = []
-    azure_list = List('data/')
-    for item in azure_list:
-        data_list.append(item.name)
+    user_id = request.args.get('user_id')
+    return user_id
     
-    return jsonify(data_list)
-        
-@app.route('/getAzureTif', methods=['POST'])
-def get_azure_tif():
-    data = []
-    azure_data = List('data/')
-    return 'lmao'
+@app.route('/homepage/undo-project', methods=['PUT'])
+def undo_project():
+    project_id = request.args.get('project_id')
+    return project_id
+
+@app.route('/homepage/delete-project', methods=['DELETE'])
+def delete_project():
+    project_id = request.args.get('project_id')
+    return project_id
+
+@app.route('/new-project', methods=['POST'])
+def new_project():
+    body_json = request.get_json()
+    print(body_json)
+    print(body_json["project_name"])
+    print(type(body_json))
     
+    message = 'created/not'
+    return message
+
+@app.route('/new-model-config', methods=['POST'])
+def new_model_config():
+    project_id = request.args.get('project_id')
+
+    return project_id
+
 @app.route('/post_form', methods=['POST'])
 def process_form():
     data = request.form
     print(data['username'])
     print(data['password'])
     return data
-
 
 if __name__ == '__main__':
     # run app in debug mode on port 5000

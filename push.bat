38750f8d7e8f81f5b74008a6348722afd1c91052
ih810 <ivanhong810@gmail.com>
2022-01-05 18:36:47 +0800

    try

--- push.bat
+++ push.bat
@@ -7,5 +7,5 @@ set /P pushing=Push?[y/[n]]:
 
 call git add .
 call git commit -m %commitMessage%
-rem
+echo commit finished
 IF %pushing%=="y" call git push origin %branchName% call git push lab origin %branchName%

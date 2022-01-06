5703a77d77993a31c207c62d33b391c021a84e75
ih810 <ivanhong810@gmail.com>
2022-01-06 11:39:27 +0800

    alias setting and ps1 file done

--- push.bat
+++ push.bat
@@ -1,13 +1,12 @@
+@echo off
 set /P commitMessage=Enter Commit Message:
-echo %commitMessage%
 set /P branchName=Enter Branch Name[option/[main]]:
 IF NOT DEFINED branchName SET "branchName=main"
-echo %branchName%
 set /P pushing=Push?[y/[n]]:
 
 call git add .
 call git commit -m '%commitMessage%'
-IF %pushing%==y (echo 'push') ELSE (echo 'nothing')
+IF %pushing%==y (echo 'push') ELSE :end
 call git push origin %branchName% 
 call git push lab %branchName%
 SET commitMessage=''
